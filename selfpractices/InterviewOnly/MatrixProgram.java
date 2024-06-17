d49f80ac6cfc42c42a7841cd4f08646d9f020d7d
vishal rai <vishalrai10797@gmail.com>
2024-06-17 18:48:18 +0530

    martris program add

---
+++ selfpractices/InterviewOnly/MatrixProgram.java
@@ -0,0 +1,19 @@
+package selfpractices.InterviewOnly;
+
+public class MatrixProgram {
+    public static void main(String args){
+
+        // Initializing a 2-D array with values
+        int[][] arr
+                = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+        // Accessing elements of 2-D array
+        System.out.println("First element of first row: "
+                + arr[0][0]);
+        System.out.println("Third element of second row: "
+                + arr[1][2]);
+        System.out.println("Second element of third row: "
+                + arr[2][1]);
+    }
+
+}
