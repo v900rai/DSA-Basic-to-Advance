659ce37c5b01dda07b9d1d8872e4af9cbbbe7f9b
vishal rai <vishalrai10797@gmail.com>
2024-07-05 19:07:05 +0530

    excpetaions solve the problem

---
+++ July2024/exceptionToString.java
@@ -0,0 +1,13 @@
+package July2024;
+
+public class exceptionToString {
+    public static void main (String[] args) {
+        int a = 5;
+        int b = 0;
+        try {
+            System.out.println(a / b);
+        } catch (ArithmeticException e) {
+            System.out.println(e.toString());
+        }
+    }
+    }
