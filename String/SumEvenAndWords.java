860ccc3a27a78a1831390e630894509b9dcf13c7
vishal rai <vishalrai10797@gmail.com>
2024-08-18 22:08:35 +0530

    hh

---
+++ String/SumEvenAndWords.java
@@ -0,0 +1,24 @@
+package String;
+
+public class SumEvenAndWords {
+    public static void printWords(String s)
+    {
+
+        // Splits Str into all possible tokens
+        for (String word : s.split(" "))
+
+            // if length is even
+            if (word.length() % 2 == 0)
+
+                // Print the word
+                System.out.println(word);
+    }
+
+    // Driver Code
+    public static void main(String[] args)
+    {
+
+        String s = "i am Geeks for Geeks and a Geek";
+        printWords(s);
+    }
+}
