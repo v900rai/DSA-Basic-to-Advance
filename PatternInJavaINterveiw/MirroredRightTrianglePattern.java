28373ec2641a436249d2959a908e1238e2c11d85
vishal rai <vishalrai10797@gmail.com>
2024-08-23 20:20:47 +0530

    add

---
+++ PatternInJavaINterveiw/MirroredRightTrianglePattern.java
@@ -0,0 +1,18 @@
+package PatternInJavaINterveiw;
+
+public class MirroredRightTrianglePattern {
+    public static void main(String args[]){
+        int n=7;
+        for(int i=0; i<=n; i++) {
+            for (int j = 1; j<= n-i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k <= i; k++) {
+                System.out.print("*");
+
+            }
+            System.out.println("");
+        }
+    }
+}
