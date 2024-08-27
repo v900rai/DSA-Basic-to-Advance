1e54257e2fd61486015e49639699c7930950221d
vishal rai <vishalrai10797@gmail.com>
2024-08-27 12:03:28 +0530

    String few program

---
+++ String/StringInPutOut.java
@@ -0,0 +1,13 @@
+package String;
+
+import java.util.Scanner;
+
+public class StringInPutOut {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String str1=sc.next();
+        String str2=sc.next();
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
