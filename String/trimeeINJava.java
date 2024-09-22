dd7de6493e5113ef5f1edee3b0d1f8b93309a095
vishal rai <vishalrai10797@gmail.com>
2024-09-22 11:13:36 +0530

    trimmm

---
+++ String/trimeeINJava.java
@@ -0,0 +1,9 @@
+package String;
+
+public class trimeeINJava {
+    public static void main(String[] args) {
+        String str="     hello vishal rai ";
+        System.out.println(str);
+        System.out.println(str.trim());// removing the strating index and removing the last space
+    }
+}
