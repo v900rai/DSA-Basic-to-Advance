a62a6cb8951d1c269ac80dac185324572558c3bf
vishal rai <vishalrai10797@gmail.com>
2024-09-22 14:35:01 +0530

    multiple inheritance

---
+++ OOPSJAVA/INHERITANCE/hjtr.java
@@ -0,0 +1,26 @@
+package OOPSJAVA.INHERITANCE;
+
+
+interface Printable{
+    void print();
+}
+interface Showable{
+    void show();
+}
+interface vishal{
+    void run();
+}
+class A7 implements Printable,Showable ,vishal{
+    public void print(){System.out.println("Hello");}
+    public void show(){System.out.println("Welcome");}
+    public void run(){
+        System.out.println("vishal rai");
+    }
+
+    public static void main(String args[]){
+        A7 obj = new A7();
+        obj.print();
+        obj.show();
+        obj.run();
+    }
+}
