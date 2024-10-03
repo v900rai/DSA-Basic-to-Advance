6d4b53b63a1568206b1e9ff2e49cc684a5bb0159
vishal rai <vishalrai10797@gmail.com>
2024-10-03 19:01:47 +0530

    bubble sort

--- DSA100Challenge/LinearSearchInJava.java
+++ DSA100Challenge/LinearSearchInJava.java
@@ -4,7 +4,7 @@ public class LinearSearchInJava {
     public static void main(String args[]){
         int arr[]={10,20,30,40,50};
         int target=30;
-        int result=linerserach(arr,target);
+        int result=binarySearch(arr,target);
         if(result != -1){
             System.out.println("Element found at  Index :"+result);
         }
@@ -17,6 +17,26 @@ public class LinearSearchInJava {
         for(int i=0; i<=arr.length; i++){
             if(arr[i]==target){
                 return i;
+            }
+        }
+        return -1;
+    }
+
+
+    private static int binarySearch(int arr[], int target){
+        int start=0;
+        int end=arr.length;
+        while(start<= end){
+            int mid=(start+end)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            if(arr[mid]<target){
+                start=mid+1;
+
+            }
+            else {
+                end=mid-1;
             }
         }
         return -1;
