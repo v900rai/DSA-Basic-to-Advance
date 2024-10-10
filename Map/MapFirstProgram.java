a2bf81706ea595d0560504ca0edf6688d44eef0f
vishal rai <vishalrai10797@gmail.com>
2024-10-10 10:58:26 +0530

    Map first program key value pair store only

---
+++ Map/MapFirstProgram.java
@@ -0,0 +1,15 @@
+package Map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapFirstProgram {
+    public static void main(String args[]){
+        Map map=new HashMap();
+        map.put(101, "vishal");
+        map.put(102, "Ajay ");
+        map.put(103, "vipul");
+        map.put(104, "ifrandus");
+        System.out.println(map);
+    }
+}
