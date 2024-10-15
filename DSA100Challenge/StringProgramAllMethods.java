c6ebd633b0bc89f129a3088086dae8b9891e3ad9
vishal rai <vishalrai10797@gmail.com>
2024-10-15 14:18:20 +0530

    yrtjyklf

---
+++ DSA100Challenge/StringProgramAllMethods.java
@@ -0,0 +1,20 @@
+package DSA100Challenge;
+
+import java.util.Scanner;
+
+public class StringProgramAllMethods {
+    public static void main(String args[]) {
+        System.out.println("Enter the elements ");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        /*char ch = 0;
+        for (int i = 0; i < str.length(); i++) {
+            ch = str.charAt(i);
+        }
+        System.out.println(" print the character  " + ch);
+*/
+
+
+    }
+}
