30da7fddc757e5bfb6b309349548ab26369206b5
vishal rai <vishalrai10797@gmail.com>
2024-11-03 12:23:19 +0530

    ThisKeyword

---
+++ November2024/THisDemo.java
@@ -0,0 +1,16 @@
+package November2024;
+
+public class THisDemo {
+    public static void main(String args[]){
+        THisDemo st=new THisDemo(100);
+
+    }
+    public THisDemo(){
+
+        System.out.println("no arg constructor");
+    }
+    public  THisDemo(int a){
+this();
+        System.out.println(" parametrised constructor ");
+    }
+}
