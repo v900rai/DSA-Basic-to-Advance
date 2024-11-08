aae40ca3efd4d756f0c18bc6ce6e7e40ddfb39e9
vishal rai <vishalrai10797@gmail.com>
2024-11-08 11:27:01 +0530

    liftShift

---
+++ November2024/leftShiftOperators.java
@@ -0,0 +1,11 @@
+package November2024;
+
+public class leftShiftOperators {
+    public static void main(String args[]){
+        int number =2;
+        // 2 bit left shift operation
+        int Ans = number << 1;
+
+        System.out.println(Ans);
+    }
+}
