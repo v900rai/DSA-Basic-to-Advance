dea3d7eae3c441aaf6d70fcd3c0075367e097593
vishal rai <vishalrai10797@gmail.com>
2024-11-08 11:32:50 +0530

    rightShiftOperators

---
+++ November2024/RightShiftOperatots.java
@@ -0,0 +1,9 @@
+package November2024;
+
+public class RightShiftOperatots {
+    public static void main(String args[]){
+        int number =8;
+        int ans=number>>3;
+        System.out.println(ans);
+    }
+}
