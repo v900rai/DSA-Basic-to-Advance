179da25c259b9fc76796b40e46657dc4ffcf07f1
vishal rai <vishalrai10797@gmail.com>
2024-11-13 10:55:07 +0530

    HashMap First Program

---
+++ Map/FirstHashMapProgram.java
@@ -0,0 +1,10 @@
+package Map;
+
+import java.util.HashMap;
+
+public class FirstHashMapProgram {
+    public static void main(String args[]){
+        HashMap<String , Integer>map=new HashMap<>();
+        System.out.println(map);
+    }
+}
