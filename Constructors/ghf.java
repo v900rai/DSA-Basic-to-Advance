4121ead83a1ef8d834b46128d755325c585e9020
vishal rai <vishalrai10797@gmail.com>
2024-11-19 22:35:16 +0530

    all

---
+++ Constructors/ghf.java
@@ -0,0 +1,24 @@
+package Constructors;
+
+
+
+class Multiply {
+
+    int a = 5;
+    int b = 5;
+
+    public int mul(int a, int b)
+    {
+        this.a = a;
+        this.b = b;
+        return a * b;
+    }
+}
+
+class Display {
+    public static void main(String[] args)
+    {
+        Multiply m = new Multiply();
+        System.out.println(m.mul(5, 5));
+    }
+}
