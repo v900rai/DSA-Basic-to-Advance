f4c4c8fcb7908fc9a9a6a94ea404c624d210eb35
vishal rai <vishalrai10797@gmail.com>
2024-11-19 22:49:45 +0530

    multidiamensional

---
+++ ArraysProgram/Multidimensional.java
@@ -0,0 +1,18 @@
+package ArraysProgram;
+
+public class Multidimensional {
+    public static void main(String[] args){
+
+        // Two Dimensional Array
+        // Declared and Initialized
+        int[][] arr = new int[3][3];
+
+
+        // Number of Rows
+        System.out.println("Rows : " + arr.length);
+
+        // Number of Columns
+        System.out.println("Columns : " + arr[0].length);
+    }
+
+}
