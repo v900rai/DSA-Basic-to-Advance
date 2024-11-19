14580879870b314cc76b1136ca59fc67d354edc8
vishal rai <vishalrai10797@gmail.com>
2024-11-19 22:39:53 +0530

    arrays lenghth

---
+++ Constructors/LengthArrays.java
@@ -0,0 +1,30 @@
+package Constructors;
+
+public class LengthArrays {
+    // Java program to illustrate creating an array
+// of integers,  puts some values in the array,
+// and prints each value to standard output.
+
+
+        public static void main(String[] args)
+        {
+            // declares an Array of integers.
+
+            // allocating memory for 5 integers.
+          int []  arr = new int[5];
+
+            // initialize the elements of the array
+            // first to last(fifth) element
+            arr[0] = 10;
+            arr[1] = 20;
+            arr[2] = 30;
+            arr[3] = 40;
+            arr[4] = 50;
+
+            // accessing the elements of the specified array
+            for (int i = 0; i < arr.length; i++)
+                System.out.println("Element at index "
+                        + i + " : " + arr[i]);
+        }
+    }
+
