440c1528a24b7168c34a0bd419ea9419f1d6b04c
vishal rai <vishalrai10797@gmail.com>
2024-12-01 18:22:23 +0530

    Variable data types

---
+++ ArraysProgram/VariableTypes.java
@@ -0,0 +1,16 @@
+package ArraysProgram;
+
+public class VariableTypes {
+
+    static int m=100;//static variable
+    void method()
+    {
+        int n=90;//local variable
+        int num=10; //local variable
+        System.out.println(num);
+    }
+    public static void main(String args[])
+    {
+        int data=50;//instance variable
+    }
+}
