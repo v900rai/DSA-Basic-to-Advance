e85e19164e10c7dd8f14d1cb806cffe8f203c938
vishal rai <vishalrai10797@gmail.com>
2024-12-02 19:18:44 +0530

    differente method

---
+++ December2024/MultipleParameters.java
@@ -0,0 +1,11 @@
+package December2024;
+
+public class MultipleParameters {
+    public static void main(String args[]){
+        mulparamerets("vishal rai ", 11);
+
+    }
+    public static void mulparamerets(String name, int age){
+        System.out.println(name +" is "+age);
+    }
+}
---
+++ December2024/VoteMethod.java
@@ -0,0 +1,16 @@
+package December2024;
+
+public class VoteMethod {
+    public static void checkAge(int age){
+        if(age<18){
+            System.out.println( "Access denied - You are not old enough!");
+        } else
+            System.out.println("Access granted - You are old enough!"); {
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        checkAge(20);
+    }
+}
