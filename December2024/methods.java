fca673d4d1f79db73759889c8f3e084750642821
vishal rai <vishalrai10797@gmail.com>
2024-12-02 19:09:03 +0530

    Methods all

---
+++ December2024/methods.java
@@ -0,0 +1,14 @@
+package December2024;
+
+public class methods {
+    public static void methodd(){
+        System.out.println(" I just got executed!");
+
+    }
+
+    public static void main(String args[]){
+        methodd();
+        methodd();
+
+    }
+}
---
+++ December2024/parameters.java
@@ -0,0 +1,13 @@
+package December2024;
+
+public class parameters {
+    public static void main(String args[]){
+        myMethod("rai");
+        myMethod(" rai");
+
+    }
+    public static void myMethod(String name){
+        System.out.println(" vishal rai "+name);
+    }
+
+}
