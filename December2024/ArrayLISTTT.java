dba074915195ec6bd0166230792f8a86f51e5c1e
vishal rai <vishalrai10797@gmail.com>
2024-12-10 17:03:07 +0530

    ArrayList

---
+++ December2024/ArrayLISTTT.java
@@ -0,0 +1,16 @@
+package December2024;
+
+import java.util.ArrayList;
+
+public class ArrayLISTTT {
+    public static void main(String[] args) {
+        ArrayList<String> colour=new ArrayList<String >();
+        colour.add("Red");
+        colour.add("Red");
+        colour.add("White");
+        colour.add("Yellow");
+        colour.add("Green");
+        System.out.println(" All colour show "+colour);
+    }
+
+}
