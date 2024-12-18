dbb641a457c25996d0ac80b99ac0d60c5d2ecbe1
vishal rai <vishalrai10797@gmail.com>
2024-12-18 19:33:00 +0530

    CodePointAt methods are return first character in a string

---
+++ JavaMultithreading/hello.java
@@ -0,0 +1,8 @@
+package JavaMultithreading;
+
+public class hello
+{
+    public static void main(String args[]){
+        System.out.println("hello vishal rai ");
+    }
+}
---
+++ StringAllMethods/CODEpointAt.java
@@ -0,0 +1,9 @@
+package StringAllMethods;
+
+public class CODEpointAt {
+    public static void main(String[] args) {
+        String str="Vishalrai";
+        int result=str.codePointAt(0);
+        System.out.println(result);
+    }
+}
