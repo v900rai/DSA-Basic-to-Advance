d93ac51565b6273ef4fa5c2f7e2c9af6e7487f4b
vishal rai <vishalrai10797@gmail.com>
2024-12-18 19:35:13 +0530

    Return the first character only

---
+++ StringAllMethods/charAttt.java
@@ -0,0 +1,9 @@
+package StringAllMethods;
+
+public class charAttt {
+    public static void main(String[] args) {
+        String myStr = "Hello";
+        char result = myStr.charAt(0);
+        System.out.println(result);
+    }
+}
