137e2fb6e7c1298b8f23ab362b15b21def0ed219
vishal rai <vishalrai10797@gmail.com>
2024-12-26 13:48:06 +0530

    PrintWithoutBlankMain Interview problem many times asked this types queustions

---
+++ Interview2025DSA/PrintWithoutBlankMain.java
@@ -0,0 +1,13 @@
+package Interview2025DSA;
+
+public class PrintWithoutBlankMain {
+    public static boolean printWithoutStaticMain(){
+        System.out.println("hello world");
+        return true;
+    }
+    static boolean p=printWithoutStaticMain();
+
+    public static void main(String[] args) {
+
+    }
+}
