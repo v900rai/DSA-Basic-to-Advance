ac60c0156d9b6996129790d23795fc160cd4d48e
vishal rai <vishalrai10797@gmail.com>
2024-12-27 12:06:07 +0530

    cheks String anagram are not

---
+++ Interview2025DSA/ChecksStringAnagramAreNot.java
@@ -0,0 +1,29 @@
+package Interview2025DSA;
+
+import java.util.Arrays;
+
+public class ChecksStringAnagramAreNot {
+    // Java Code to check if two Strings are anagrams of
+// each other using sorting
+
+
+        // Function to check if two strings are anagrams
+        static boolean areAnagrams(String s1, String s2) {
+
+            // Sort both strings
+            char[] s1Array = s1.toCharArray();
+            char[] s2Array = s2.toCharArray();
+            Arrays.sort(s1Array);
+            Arrays.sort(s2Array);
+
+            // Compare sorted strings
+            return Arrays.equals(s1Array, s2Array);
+        }
+
+        public static void main(String[] args) {
+            String s1 = "geeks";
+            String s2 = "kseeg";
+            System.out.println(areAnagrams(s1, s2));
+        }
+    }
+
