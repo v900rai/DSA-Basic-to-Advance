515d008351866726daeca0e73e7be0de19ff290f
vishal rai <vishalrai10797@gmail.com>
2025-01-03 19:07:07 +0530

    String builder insert method are used

---
+++ Interview2025DSA/StringBuilderInsertMethodss.java
@@ -0,0 +1,11 @@
+package Interview2025DSA;
+
+public class StringBuilderInsertMethodss {
+    public static void main(String[] args) {
+        {
+            StringBuilder sb = new StringBuilder("Hello");
+            sb.insert(5, " Java");
+            System.out.println(sb); // Output: Hello Java
+        }
+    }
+}
