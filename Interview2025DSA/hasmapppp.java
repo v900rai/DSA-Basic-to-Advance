1ba5fa003c5eb898ab543d164adf7c959b6c3c2f
vishal rai <vishalrai10797@gmail.com>
2025-01-03 11:26:58 +0530

    HashMap

---
+++ Interview2025DSA/hasmapppp.java
@@ -0,0 +1,15 @@
+package Interview2025DSA;
+import java.util.HashMap;
+public class hasmapppp {
+
+
+
+        public static void main(String[] args) {
+            HashMap<String, Integer> map = new HashMap<>();
+            map.put("Apple", 3);
+            map.put("Banana", 5);
+            System.out.println(map.get("Apple")); // Output: 3
+        }
+    }
+
+
