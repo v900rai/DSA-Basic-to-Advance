7cd4bea0b1fd0dd8f9cc5a977ce468237cb65da7
vishal rai <vishalrai10797@gmail.com>
2025-01-03 18:36:07 +0530

    String to charachter

---
+++ Interview2025DSA/StringToCharacter.java
@@ -0,0 +1,18 @@
+package Interview2025DSA;
+
+public class StringToCharacter {
+    public static void main(String[] args) {
+        String str="Hello";
+        char ch[]=str.toCharArray();
+        for (char c : ch) {
+            System.out.println(c);
+        }
+    }
+}
+/*
+//output
+H
+        e
+l
+        l
+o*/
