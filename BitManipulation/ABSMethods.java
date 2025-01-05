fde23968056f3a5c5807bb4d664c83b19075f26a
vishal rai <vishalrai10797@gmail.com>
2025-01-06 00:27:13 +0530

    Absolute of value

---
+++ BitManipulation/ABSMethods.java
@@ -0,0 +1,17 @@
+package BitManipulation;
+
+public class ABSMethods {
+
+        public static void main(String[] args) {
+            int num1 = -10;
+            double num2 = -3.14;
+            int num3 = 20;
+
+            // Using Math.abs
+            System.out.println("Absolute of " + num1 + " is: " + Math.abs(num1)); // 10
+            System.out.println("Absolute of " + num2 + " is: " + Math.abs(num2)); // 3.14
+            System.out.println("Absolute of " + num3 + " is: " + Math.abs(num3)); // 20
+        }
+    }
+
+
