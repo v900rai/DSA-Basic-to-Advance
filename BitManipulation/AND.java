402a1d87a389199d645df2f6c1f1250ffd463d2e
vishal rai <vishalrai10797@gmail.com>
2025-01-05 15:46:14 +0530

    And operators

---
+++ BitManipulation/AND.java
@@ -0,0 +1,10 @@
+package BitManipulation;
+
+public class AND {
+    public static void main(String[] args) {
+        int a = 5;  // 0101
+        int b = 3;  // 0011
+        int result = a & b; // 0001 (1 in decimal)
+        System.out.println(result); // Output: 1
+    }
+}
