d9f2650dea86cc7ac6f16371a329f49ff679960c
vishal rai <vishalrai10797@gmail.com>
2025-01-05 15:49:46 +0530

    OR

---
+++ BitManipulation/OR.java
@@ -0,0 +1,11 @@
+package BitManipulation;
+
+public class OR {
+    public static void main(String[] args) {
+        int a = 5;  // 0101
+        int b = 3;  // 0011
+        int result = a | b; // 0111 (7 in decimal)
+        System.out.println(result); // Output: 7
+
+    }
+}
