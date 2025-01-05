c4ba23e99f7723d9a833d7feefaeebe8e269643a
vishal rai <vishalrai10797@gmail.com>
2025-01-05 16:39:14 +0530

    Using terb=nary oprratirs

---
+++ BitManipulation/TernaryOperatorSyntax.java
@@ -0,0 +1,13 @@
+package BitManipulation;
+
+public class TernaryOperatorSyntax {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+// Using Ternary Operator
+        int max = (a > b) ? a : b;
+
+        System.out.println("Maximum: " + max); // Output: Maximum: 20
+
+    }
+}
