a4a9b04b6ab6015222934008d55285b35106166d
vishal rai <vishalrai10797@gmail.com>
2025-01-05 15:55:26 +0530

    Xor ka matlb same same zero ho jata h but diffrent jo hota h oo one ho jata

---
+++ BitManipulation/XOR.java
@@ -0,0 +1,11 @@
+package BitManipulation;
+
+public class XOR {
+    public static void main(String[] args) {
+        int a = 5;  // 0101
+        int b = 3;  // 0011
+        int result = a ^ b; // 0110 (6 in decimal)
+        System.out.println(result); // Output: 6
+
+    }
+}
