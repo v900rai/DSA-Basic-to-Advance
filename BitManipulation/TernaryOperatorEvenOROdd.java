537d2d45ad3766372aadcdc1dbf68094f7e4a1b4
vishal rai <vishalrai10797@gmail.com>
2025-01-05 16:41:42 +0530

    Ternary Operatotrs Using Even odd number

---
+++ BitManipulation/TernaryOperatorEvenOROdd.java
@@ -0,0 +1,13 @@
+package BitManipulation;
+
+public class TernaryOperatorEvenOROdd {
+    public static void main(String[] args) {
+        int num = 7;
+
+// Using Ternary Operator
+        String result = (num % 2 == 0) ? "Even" : "Odd";
+
+        System.out.println("The number is: " + result); // Output: The number is: Odd
+
+    }
+}
