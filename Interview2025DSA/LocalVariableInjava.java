d2a5ca7ca906732e48b82b8f7aead7e3a0dcafe0
vishal rai <vishalrai10797@gmail.com>
2025-01-07 17:26:05 +0530

    local variable

---
+++ Interview2025DSA/LocalVariableInjava.java
@@ -0,0 +1,13 @@
+package Interview2025DSA;
+
+public class LocalVariableInjava {
+    public static void main(String[] args) {
+        // Declared a Local Variable
+        int var = 10;
+
+        // This variable is local to this main method only
+        System.out.println("Local Variable: " + var);
+    }
+
+
+}
