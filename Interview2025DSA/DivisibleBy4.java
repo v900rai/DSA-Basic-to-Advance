53af1922ee22b528412f3a37127d7691d0406a5f
vishal rai <vishalrai10797@gmail.com>
2025-01-09 18:04:10 +0530

    divisible by 4

---
+++ Interview2025DSA/DivisibleBy4.java
@@ -0,0 +1,17 @@
+package Interview2025DSA;
+
+public class DivisibleBy4 {
+    public static void main(String[] args) {
+        long n=123456758933l;
+
+        // finding given number is divisible by 4 or not
+        if (n % 4 == 0)
+        {
+            System.out.println("Yes");
+        }
+        else
+        {
+            System.out.println("No");
+        }
+    }
+}
