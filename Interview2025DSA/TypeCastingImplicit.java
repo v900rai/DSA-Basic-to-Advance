890153caa6570805ab94bb53dc693d1e50987272
vishal rai <vishalrai10797@gmail.com>
2025-01-11 18:55:42 +0530

    implicity

---
+++ Interview2025DSA/TypeCastingImplicit.java
@@ -0,0 +1,10 @@
+package Interview2025DSA;
+
+public class TypeCastingImplicit {
+    public static void main(String[] args) {
+        int a=10;//4 bytes
+        double b=a; //8bytes
+        System.out.println(b);
+
+    }
+}
