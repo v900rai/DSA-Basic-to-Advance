6d4925217c0fdda47b01948e1e348a8bcd557d6e
vishal rai <vishalrai10797@gmail.com>
2025-01-12 14:45:14 +0530

    polymorphism using multiple interface in java

---
+++ Interview2025DSA/polymorishm.java
@@ -0,0 +1,29 @@
+package Interview2025DSA;
+//Using Interface with Polymorphism
+
+interface Animal {
+    void sound();
+}
+
+class Dog implements Animal {
+    public void sound() {
+        System.out.println("Dog says: Bark");
+    }
+}
+
+class Cat implements Animal {
+    public void sound() {
+        System.out.println("Cat says: Meow");
+    }
+}
+
+public class polymorishm {
+    public static void main(String[] args) {
+        Animal myAnimal; // Interface reference
+        myAnimal = new Dog();
+        myAnimal.sound(); // Output: Dog says: Bark
+
+        myAnimal = new Cat();
+        myAnimal.sound(); // Output: Cat says: Meow
+    }
+}
