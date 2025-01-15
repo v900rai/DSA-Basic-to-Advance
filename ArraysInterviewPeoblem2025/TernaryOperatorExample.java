10aa2af86b65f765f185fd6572fc93ed8c1c944c
vishal rai <vishalrai10797@gmail.com>
2025-01-15 22:03:14 +0530

    Bollean logics i used right now to tarnary operators in java i love coding hahaha

---
+++ ArraysInterviewPeoblem2025/TernaryOperatorExample.java
@@ -0,0 +1,9 @@
+package ArraysInterviewPeoblem2025;
+
+public class TernaryOperatorExample {
+    public static void main(String[] args) {
+        boolean conditions=true;
+        String result=(conditions)? "true":"false";
+        System.out.println(result);
+    }
+}
