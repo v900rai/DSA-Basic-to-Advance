76f11657e100eca059d8abfa969bc0aa019b4667
vishal rai <vishalrai10797@gmail.com>
2025-01-15 21:51:38 +0530

    res = (n1 > n2) ? (n1 + n2) : (n1 - n2);

---
+++ ArraysInterviewPeoblem2025/SecondMethosTernaryOperator.java
@@ -0,0 +1,18 @@
+package ArraysInterviewPeoblem2025;
+
+public class SecondMethosTernaryOperator {
+    public static void main(String[] args) {
+
+        // variable declaration
+        int n1 = 5, n2 = 10, res;
+
+        System.out.println("First num: " + n1);
+        System.out.println("Second num: " + n2);
+
+        // Performing ternary operation
+        res = (n1 > n2) ? (n1 + n2) : (n1 - n2);
+
+        // Print the largest number
+        System.out.println("Result = " + res);
+    }
+}
