e1bcdd4df442639373a5cb17b20c3c338d30886a
vishal rai <vishalrai10797@gmail.com>
2025-01-18 19:39:57 +0530

    Interfaces can now include static methods. These methods are called directly using the interface name and are not inherited by implementing classes.

---
+++ ArraysInterviewPeoblem2025/InterfaceMultipleSecondMethods.java
@@ -0,0 +1,15 @@
+package ArraysInterviewPeoblem2025;
+
+interface TestInterface
+{
+    final int a = 10;
+    static void display()
+    {
+        System.out.println("hello");
+    }
+}
+public class InterfaceMultipleSecondMethods {
+    public static void main(String args[]){
+        TestInterface.display();
+    }
+}
