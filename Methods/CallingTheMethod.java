4d0c071af58783f613174875fac836721bc10e9d
vishal rai <vishalrai10797@gmail.com>
2025-01-21 18:51:35 +0530

    Calling the methods in java a

---
+++ Methods/CallingTheMethod.java
@@ -0,0 +1,19 @@
+package Methods;
+
+public class CallingTheMethod {
+    // Static method
+    static void hello() {
+        System.out.println("Hello");
+    }
+}
+
+    class nn {
+
+        public static void main(String[] args) {
+
+            // calling the Method 1
+            // Accessing method
+            CallingTheMethod.hello();
+        }
+    }
+
