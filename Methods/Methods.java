30bc590d62924aeb7cd8adec712496949a446847
vishal rai <vishalrai10797@gmail.com>
2025-01-21 18:34:12 +0530

    Methods basics

---
+++ Methods/Methods.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class Methods {
+    public static void display(){
+        System.out.println("Display the methods ");
+    }
+
+    public static void main(String[] args) {
+        Methods.display();
+    }
+}
