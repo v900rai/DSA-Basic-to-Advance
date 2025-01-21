b8e1a646f813333639a947058165dc9b4ebfd2a8
vishal rai <vishalrai10797@gmail.com>
2025-01-21 18:44:55 +0530

    Hashcod print

---
+++ Methods/HashCode.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class HashCode {
+    public static void main(String[] args) {
+
+        // Creating object of the class in
+        // main() method
+        GFG ob = new GFG();
+
+        // Calling predefined method
+        System.out.println(ob.hashCode());
+    }
+}
