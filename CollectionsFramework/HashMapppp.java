3fa63c9cd4d90be0fe1befafd64db83778ead62a
vishal rai <vishalrai10797@gmail.com>
2025-01-24 11:55:44 +0530

    Mam creating hash map and also printing displaying the

---
+++ CollectionsFramework/HashMapppp.java
@@ -0,0 +1,14 @@
+package CollectionsFramework;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapppp {
+    public static void main(String args[]){
+        Map<String , Integer> map=new HashMap<>();
+        // creating hash map
+        System.out.println("displaying tha map "+map);
+        // i am creating empty hash map
+
+    }
+}
