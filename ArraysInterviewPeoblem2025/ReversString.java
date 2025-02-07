23d7e4f1832b2a33d32ed44cbeb85cf304dee249
vishal rai <vishalrai10797@gmail.com>
2025-02-07 14:59:45 +0530

    Add String Builder method also

--- ArraysInterviewPeoblem2025/ReversString.java
+++ ArraysInterviewPeoblem2025/ReversString.java
@@ -5,8 +5,17 @@ public class ReversString {
         String str="vishal";
         char ch[]=str.toCharArray();
         for(int i=ch.length-1; i>=0; i--){
-            System.out.print(ch[i]+" ,");
+            System.out.print(ch[i]);
         }
+        //
+        System.out.println(".......................");
+        for(int i=str.length()-1; i>=0; i--){
+            System.out.println("  ");
+            System.out.print(str.charAt(i));
+        }
+        //approach 3
+        StringBuilder st=new StringBuilder(str);
+        System.out.println(st.reverse());
 
     }
 }
