7cc94b72e52be273bbef61696dd32845bda93bf9
vishal rai <vishalrai10797@gmail.com>
2025-02-15 11:56:06 +0530

    hashSet

---
+++ BitManipulation/HasssSett.java
@@ -0,0 +1,16 @@
+package BitManipulation;
+
+import java.util.HashSet;
+
+public class HasssSett {
+    public static void main(String args[]){
+        HashSet<Integer> lis=new HashSet<>();
+        lis.add(22);
+        lis.add(23);
+        lis.add(24);
+        System.out.println(lis);
+        if(lis.contains(22)){
+            System.out.println();
+        }
+    }
+}
