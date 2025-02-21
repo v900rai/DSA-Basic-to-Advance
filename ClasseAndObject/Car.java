a06fbc0ecda98e0d9f5512c75d490465ef59d117
vishal rai <vishalrai10797@gmail.com>
2025-02-21 11:31:52 +0530

    Class and object

---
+++ ClasseAndObject/Car.java
@@ -0,0 +1,23 @@
+package ClasseAndObject;
+
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class Car {
+    int year;
+    String name;
+
+    public void show(){
+        System.out.println("Car is Running ");
+    }
+
+    public static void main(String[] args) {
+        Car cr=new Car();
+        System.out.println("Car "+cr.name+" "+cr.year);
+        cr.year=1992;
+        cr.name="BMW";
+        cr.show();
+
+        System.out.println("Year "+cr.year+"Name "+cr.name);
+    }
+
+}
