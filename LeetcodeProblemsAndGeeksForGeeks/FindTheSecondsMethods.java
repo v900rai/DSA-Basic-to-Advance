8853579a3d193781edaac0d08cfc0e7c2c4ee911
vishal rai <vishalrai10797@gmail.com>
2025-02-21 19:37:56 +0530

    find the Second methods but recursive methods

---
+++ LeetcodeProblemsAndGeeksForGeeks/FindTheSecondsMethods.java
@@ -0,0 +1,12 @@
+package LeetcodeProblemsAndGeeksForGeeks;
+
+public class FindTheSecondsMethods {
+    public static int summm(int n){
+        return n*(n+1)/2;
+    }
+
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(summm(n));
+    }
+}
