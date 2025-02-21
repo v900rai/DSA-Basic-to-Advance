ac2b51432b45d484753827ffc8ae4d321c5a8c92
vishal rai <vishalrai10797@gmail.com>
2025-02-21 11:58:17 +0530

    FindUniqueCharacterInStringLeetcode

--- ClasseAndObject/Car.java
+++ ClasseAndObject/Car.java
@@ -6,18 +6,18 @@ public class Car {
     int year;
     String name;
 
-    public void show(){
+    public void show() {
         System.out.println("Car is Running ");
     }
 
     public static void main(String[] args) {
-        Car cr=new Car();
-        System.out.println("Car "+cr.name+" "+cr.year);
-        cr.year=1992;
-        cr.name="BMW";
+        Car cr = new Car();
+        System.out.println("Car " + cr.name + " " + cr.year);
+        cr.year = 1992;
+        cr.name = "BMW";
         cr.show();
 
-        System.out.println("Year "+cr.year+"Name "+cr.name);
+        System.out.println("Year " + cr.year + "Name " + cr.name);
     }
 
 }
---
+++ LeetcodeProblemsAndGeeksForGeeks/FindUniqueCharacterInStringLeetcode.java
@@ -0,0 +1,25 @@
+package LeetcodeProblemsAndGeeksForGeeks;
+
+public class FindUniqueCharacterInStringLeetcode {
+    public static void main(String[] args) {
+        System.out.println(findUniqueCharacter("leetcode"));
+    }
+
+    public static int  findUniqueCharacter(String str){
+        int freq[]=new int[26];
+        char chars[]=str.toCharArray();
+        for(char ch:chars){
+            freq[ch-'a']++;
+
+        }
+        for(int i=0; i<chars.length; i++){
+            if(freq[chars[i]-'a']==1){
+                return i;
+            }
+        }
+        return -1;
+
+    }
+
+}
+
