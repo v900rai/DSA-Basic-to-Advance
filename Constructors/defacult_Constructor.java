0bc27b14256e9da0530d6660a00f2a14bff1b2a3
vishal rai <vishalrai10797@gmail.com>
2025-02-24 00:17:26 +0530

    default constructor

---
+++ Constructors/defacult_Constructor.java
@@ -0,0 +1,13 @@
+package Constructors;
+
+public class defacult_Constructor {
+
+    defacult_Constructor(){
+        System.out.println("default Constructor ");
+    }
+    public static void main(String[] args) {
+        defacult_Constructor sc=new defacult_Constructor();
+
+    }
+
+}
