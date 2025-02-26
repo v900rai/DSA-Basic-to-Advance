cdc7991ba7228bb19f155a7da1f28b0bfffd167a
vishal rai <vishalrai10797@gmail.com>
2025-02-26 12:27:49 +0530

    map

--- Map/FirstHashMapProgram.java
+++ Map/FirstHashMapProgram.java
@@ -3,8 +3,11 @@ package Map;
 import java.util.HashMap;
 
 public class FirstHashMapProgram {
+
+    //Empty map
     public static void main(String args[]){
         HashMap<String , Integer>map=new HashMap<>();
-        System.out.println(map);
+        // Displaying the Map
+        System.out.println("Map elements: " + map);
     }
 }
