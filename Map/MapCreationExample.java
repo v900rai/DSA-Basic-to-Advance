b1dbf3d6db87c6b321c4577e7499b7f024120c19
vishal rai <vishalrai10797@gmail.com>
2025-02-26 12:29:49 +0530

    HashMap creating

---
+++ Map/MapCreationExample.java
@@ -0,0 +1,17 @@
+package Map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapCreationExample {
+
+    public static void main(String[] args)
+    {
+
+        // Create a Map using HashMap
+        Map<String, Integer> map = new HashMap<>();
+
+        // Displaying the Map
+        System.out.println("Map elements: " + map);
+    }
+}
