8c02934f41140e2f88580ae37185736b06045426
vishal rai <vishalrai10797@gmail.com>
2025-02-27 14:10:29 +0530

    Search the elements this is seconds methods but i used recursions code is optimize

---
+++ LinkedListDSA/SecondMethodsRecursionSearchtheElementsLinkedList.java
@@ -0,0 +1,33 @@
+package LinkedListDSA;
+
+public class SecondMethodsRecursionSearchtheElementsLinkedList {
+    public static boolean isSearchKey(Node head, int key) {
+        //check whether is key present arr not
+        if (head == null) {
+            return false;
+        }
+        // if key is present in current node the u can return the true;
+        if (head.data == key) {
+            return true;
+        }
+
+        // Recur for remaining list
+        return isSearchKey(head.next, key);
+    }
+
+    public static void main(String[] args) {
+        // Create a hard-coded linked list:
+        // 14 -> 21 -> 13 -> 30 -> 10
+        Node head = new Node(14);
+        head.next = new Node(21);
+        head.next.next = new Node(13);
+        head.next.next.next = new Node(30);
+        head.next.next.next.next = new Node(10);
+
+        // Key to search in the linked list
+        int key = 14;
+
+        if (isSearchKey(head, key)) System.out.println("Yes");
+        else System.out.println("No");
+    }
+}
