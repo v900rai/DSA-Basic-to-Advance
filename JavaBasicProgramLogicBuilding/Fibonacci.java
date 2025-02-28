4c6b2dc15691ea3def6ed524b9d0fd6b861eafc6
vishal rai <vishalrai10797@gmail.com>
2025-02-28 22:04:32 +0530

    fibonacci

---
+++ JavaBasicProgramLogicBuilding/Fibonacci.java
@@ -0,0 +1,16 @@
+package JavaBasicProgramLogicBuilding;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n=10;
+        int a=0;
+        int b=1;
+        for(int i=0; i<n; i++){
+            System.out.println(a+" ");
+            int c=a+b;
+            a=b;
+            b=c;
+
+        }
+    }
+}
