b20ec743ad628a395e357028dea1eddf63a861c1
vishal rai <vishalrai10797@gmail.com>
2025-03-04 15:49:37 +0530

    two d matrix basic

---
+++ ArraysInterviewPeoblem2025/TwoDMatrix.java
@@ -0,0 +1,16 @@
+package ArraysInterviewPeoblem2025;
+
+public class TwoDMatrix {
+    public static void main(String[] args) {
+
+        // Array Intialised and Assigned
+        int[][] arr = { { 1, 2 }, { 3, 4 } };
+
+        // Printing the Array
+        for (int i = 0; i < 2; i++){
+            for (int j = 0; j < 2; j++)
+                System.out.print(arr[i][j]+" ");
+            System.out.println();
+        }
+    }
+}
