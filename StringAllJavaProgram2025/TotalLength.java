80f41e2eb0e6852d358c77ae35d8fec9f55fc86f
vishal rai <vishalrai10797@gmail.com>
2025-03-05 11:51:30 +0530

    calculate the total length

---
+++ StringAllJavaProgram2025/TotalLength.java
@@ -0,0 +1,9 @@
+package StringAllJavaProgram2025;
+
+public class TotalLength {
+    public static void main(String[] args) {
+        String str="vishal rai";
+        System.out.println(str.length());
+    }
+
+}
