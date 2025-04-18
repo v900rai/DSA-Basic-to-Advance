aa01b02770fc895c4cd8b786539a102deb5fbb13
vishal rai <vishalrai10797@gmail.com>
2025-04-18 11:34:00 +0530

    Differance between parameters and arrugments

---
+++ ArraysTop50InterveiwProblemsGFG/ParametersAndArrugmentsDifference.java
@@ -0,0 +1,12 @@
+package ArraysTop50InterveiwProblemsGFG;
+
+public class ParametersAndArrugmentsDifference {
+    public static void greet (String name){
+        System.out.println("Name "+ name); //this is a parameters
+
+    }
+
+    public static void main(String[] args) {
+        greet("Vishal rai ");
+    }
+}
