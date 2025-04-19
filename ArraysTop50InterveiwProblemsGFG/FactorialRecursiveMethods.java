cf27d35cdfb998aa1fc93d5dcf23a121309a54e9
vishal rai <vishalrai10797@gmail.com>
2025-04-19 16:32:57 +0530

    pp87oiftydv

--- ArraysTop50InterveiwProblemsGFG/FactorialRecursiveMethods.java
+++ ArraysTop50InterveiwProblemsGFG/FactorialRecursiveMethods.java
@@ -7,8 +7,17 @@ public class FactorialRecursiveMethods {
          }
          return nums * fact(nums-1);
      }
-     public static void main(String args[]){
+   public static int factorial(int n)
+    {
+
+        // single line to find factorial
+        return (n == 1 || n == 0) ? 1
+                : n * factorial(n - 1);
+    }
+
+    public static void main(String args[]){
          int n=5;
          System.out.println(fact(n));
+        System.out.println(factorial(n));
      }
 }
