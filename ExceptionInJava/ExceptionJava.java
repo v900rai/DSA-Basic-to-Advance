1d7978b61ae32951ba0df28f987885aa2d3f0ee6
vishal rai <vishalrai10797@gmail.com>
2025-04-29 17:44:00 +0530

    ArithmeticExceptions

--- ExceptionInJava/ExceptionJava.java
+++ ExceptionInJava/ExceptionJava.java
@@ -1,4 +1,16 @@
 package ExceptionInJava;
 
 public class ExceptionJava {
+    public static void main(String[] args)
+    {
+        int n = 10;
+        int m = 0;
+        int j=2;
+
+        int ans = n / m;
+        int result=n/j;
+
+        System.out.println("Answer: " + ans);
+        System.out.println("Answer : "+result);
+    }
 }
