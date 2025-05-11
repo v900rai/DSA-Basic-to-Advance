9ee80699acf402fc291265a5a00beb89d5dcdbff
vishal rai <vishalrai10797@gmail.com>
2025-05-11 18:46:52 +0530

    restart my coding

---
+++ RestartCodingfocus11052025/Om.java
@@ -0,0 +1,8 @@
+package RestartCodingfocus11052025;
+
+public class Om {
+    public static void main(String arg[]){
+        System.out.println("Hello Mr.  Vishal Rai reset and restart and refocus my coding languages ");
+
+    }
+}
