40572046cf5d2879abeec6edb90efaeffc45127a
vishal rai <vishalrai10797@gmail.com>
2025-05-11 18:41:15 +0530

    ArrayList basic informations knowlage

---
+++ ArraysProgram/GoalsApplications.java
@@ -0,0 +1,17 @@
+package ArraysProgram;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GoalsApplications {
+    public static void main(String args[]){
+          Arrays.asList(1, 2, 3, 4);
+          Arrays.asList(1, 2, 3, 4);
+          Arrays.asList(1, 2, 3, 4);
+          Arrays.asList(1, 2, 3, 4);
+
+
+
+
+    }
+}
