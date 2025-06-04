64d3ccd4a2eb4146e9cd5c67298e4a1149c42881
vishal rai <vishalrai10797@gmail.com>
2025-06-04 15:19:35 +0530

    mathmetices

---
+++ June2025/maths.java
@@ -0,0 +1,12 @@
+package June2025;
+
+public class maths {
+    public static void main(String args[]){
+        int time = 20;
+        if (time < 18) {
+            System.out.println("Good day.");
+        } else {
+            System.out.println("Good evening.");
+        }
+    }
+}
