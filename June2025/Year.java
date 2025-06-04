3d95e21f444d6686eb81eed592c9c626b9579f33
vishal rai <vishalrai10797@gmail.com>
2025-06-04 15:32:41 +0530

    year

---
+++ June2025/Year.java
@@ -0,0 +1,12 @@
+package June2025;
+
+public class Year {
+    public static void main(String[] args) {
+        int count=3;
+        while(count>3){
+            System.out.println(count);
+            count--;
+        }
+        System.out.println(" Happy new year !! ");
+    }
+}
