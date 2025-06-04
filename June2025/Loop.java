c0099890bde7548cbaa026d3a5bcfd2a00ce4ee8
vishal rai <vishalrai10797@gmail.com>
2025-06-04 15:39:08 +0530

    oiuytjydrtsxc

---
+++ June2025/Loop.java
@@ -0,0 +1,12 @@
+package June2025;
+
+public class Loop {
+    public static void main(String args[]){
+        for(int i=0; i<10; i++){
+            if(i==4){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
