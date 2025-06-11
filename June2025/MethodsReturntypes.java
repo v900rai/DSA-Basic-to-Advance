73aefb3aefc1be97d6f4e5eb7917bfe17c1302ec
vishal rai <vishalrai10797@gmail.com>
2025-06-11 11:14:58 +0530

    return types methods

---
+++ June2025/MethodsReturntypes.java
@@ -0,0 +1,13 @@
+package June2025;
+
+public class MethodsReturntypes {
+    public static void main(String[] args) {
+       int y= myMethod(5);
+        System.out.println(y);
+
+
+    }
+    public static int myMethod(int x){
+        return 7+x;
+    }
+}
