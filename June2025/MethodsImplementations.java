712d0935a319b91ee45c5f4423a497c8a3778348
vishal rai <vishalrai10797@gmail.com>
2025-06-11 11:01:13 +0530

    few implementations

---
+++ June2025/MethodsImplementations.java
@@ -0,0 +1,12 @@
+package June2025;
+
+public class MethodsImplementations {
+    public static void myMethod(){
+        System.out.println("I just got executed ...");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+    }
+}
