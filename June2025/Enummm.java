0483ce9d2f0bf957e3005061fa8236b34e1ec4bb
vishal rai <vishalrai10797@gmail.com>
2025-06-12 18:39:47 +0530

    Enum .. enum is a traversal to tne whole arrays

---
+++ June2025/Enummm.java
@@ -0,0 +1,15 @@
+package June2025;
+enum Level {
+    LOW,
+    MEDIUM,
+    HIGH,
+    NUMBER
+}
+public class Enummm {
+        public static void main(String[] args) {
+            for (Level myVar : Level.values()) {
+                System.out.println(myVar);
+            }
+        }
+    }
+
