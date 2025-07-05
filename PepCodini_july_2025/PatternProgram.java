dc8454e77b97de1ff35959d8b7f4191c61f3d276
vishal rai <vishalrai10797@gmail.com>
2025-07-05 20:16:18 +0530

    just i am checking my program is working or not

---
+++ PepCodini_july_2025/PatternProgram.java
@@ -0,0 +1,15 @@
+package PepCodini_july_2025;
+
+public class PatternProgram {
+    public static void main(String[] args) {
+
+
+        int nums = 5;
+        for (int i = 1; i <= nums; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("  Vishal ");
+            }
+            System.out.println();
+        }
+    }
+}
