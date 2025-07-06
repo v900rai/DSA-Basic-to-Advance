bce380cc5c99df843da75a1ab54b403ae8ff0664
vishal rai <vishalrai10797@gmail.com>
2025-07-06 13:46:26 +0530

    my first excpations

---
+++ PepCodini_july_2025/FirstExcpation.java
@@ -0,0 +1,13 @@
+package PepCodini_july_2025;
+
+public class FirstExcpation {
+    public static void main(String args[]){
+
+        int n = 10;
+        int m = 0;
+
+        int ans = n / m;
+
+        System.out.println("Answer: " + ans);
+    }
+}
