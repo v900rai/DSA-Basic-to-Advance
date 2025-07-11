e1a42c73d2e97ea4aceba03c1a31aef9453945bd
vishal rai <vishalrai10797@gmail.com>
2025-07-11 12:30:02 +0530

    calculate the length

---
+++ InterviewString2025/HowToCalculateTheLength.java
@@ -0,0 +1,8 @@
+package InterviewString2025;
+
+public class HowToCalculateTheLength {
+    public static void main(String[] args) {
+        String str = "vishalrai";
+        System.out.println("Length of the string is: " + str.length());
+    }
+}
