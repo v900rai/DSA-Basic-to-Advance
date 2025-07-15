16251089638f19714a061f2ee5c1f3a6deec53e7
vishal rai <vishalrai10797@gmail.com>
2025-07-15 13:36:47 +0530

    sum of arrays

---
+++ ArraysInterviewPeoblem2025/Sum.java
@@ -0,0 +1,11 @@
+package ArraysInterviewPeoblem2025;
+
+public class Sum {
+    public static void main(String[] args) {
+        int sum=0;
+        for(int i=0; i<5; i++){
+            sum=sum+i;
+        }
+        System.out.println(sum +" find the sum  ");
+    }
+}
