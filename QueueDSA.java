c286f516328004c4a80b7919d7f2cb283666332d
vishal rai <vishalrai10797@gmail.com>
2025-07-19 19:26:14 +0530

    queue implementations here

---
+++ QueueDSA.java
@@ -0,0 +1,2 @@
+public class QueueDSA {
+}
---
+++ QueueDSA2025/FirstProgram.java
@@ -0,0 +1,7 @@
+package QueueDSA2025;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println(" hello vishal rai today i will discus queue data structure all methods  ");
+    }
+}
