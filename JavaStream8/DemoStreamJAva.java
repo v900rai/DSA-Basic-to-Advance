d45f930041030ae5c41057b0bc801fdcf0a1098d
vishal rai <vishalrai10797@gmail.com>
2025-07-22 16:48:48 +0530

    collections collection

---
+++ JavaStream8/DemoStreamJAva.java
@@ -0,0 +1,9 @@
+package JavaStream8;
+
+public class DemoStreamJAva {
+    public static void main(String[] args) {
+        // streams
+        // java 8 .... > minimal code .. functional interface
+        // lambda expression , Streams,
+    }
+}
