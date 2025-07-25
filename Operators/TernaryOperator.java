7c5cad36530d3f7d24cb0634a8f475114381cd62
vishal rai <vishalrai10797@gmail.com>
2025-07-25 11:26:05 +0530

    ternary operator using some basic concepts hera i know about

---
+++ Operators/TernaryOperator.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class TernaryOperator {
+    public static void main(String args[]){
+        int number = -10;
+        String result = (number > 0) ? "Positive" : "Non-positive";
+        System.out.println("Number is: " + result);
+
+    }
+}
