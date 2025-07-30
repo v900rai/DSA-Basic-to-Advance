c9d477a8d4cdfde347c450d04c80d71af184938b
vishal rai <vishalrai10797@gmail.com>
2025-07-30 12:05:16 +0530

    methods calling parametrs

---
+++ methodspratices2025/MathOperation.java
@@ -0,0 +1,17 @@
+package methodspratices2025;
+
+public class MathOperation {
+    public static void main(String args[]){
+        add(10,20);
+        sub(30, 10);
+
+    }
+    public static  void sub( int a, int b){
+        System.out.println(" sub is :"+(a-b));
+    }
+
+    public static void add( int a, int b){
+        System.out.println(" Sum is :"+(a+b));
+
+    }
+}
---
+++ methodspratices2025/methods.java
@@ -0,0 +1,11 @@
+package methodspratices2025;
+
+public class methods {
+    public static void main(String arsg[]){
+        greet();
+
+    }
+    public static void greet(){
+        System.out.println(" hello vishal rai ");
+    }
+}
