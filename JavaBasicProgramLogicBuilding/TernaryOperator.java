01d0eb3e54ba6f79731b59b0dac805d6cff0f4c1
vishal rai <vishalrai10797@gmail.com>
2025-10-03 16:33:39 +0530

    ternary operators

---
+++ JavaBasicProgramLogicBuilding/TernaryOperator.java
@@ -0,0 +1,19 @@
+package JavaBasicProgramLogicBuilding;
+
+public class TernaryOperator {
+    // Java program to illustrate
+// max of three numbers using
+// ternary operator.
+
+
+        public static void main(String[] args)
+        {
+            int a = 20, b = 10, c = 30, result;
+
+            // result holds max of three
+            // numbers
+            result = ((a > b) ? (a > c) ? a : c : (b > c) ? b : c);
+            System.out.println("Max of three numbers = "+ result);
+        }
+
+}
