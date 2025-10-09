34a940fb7f4aebc0ead623d86cc6df3620640b90
vishal rai <vishalrai10797@gmail.com>
2025-10-09 19:40:55 +0530

    pattern

---
+++ JavaBasicProgramLogicBuilding/ReversPattern.java
@@ -0,0 +1,13 @@
+package JavaBasicProgramLogicBuilding;
+
+public class ReversPattern {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=n; i>=0; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
