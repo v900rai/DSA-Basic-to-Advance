8f61181ecff74b75fad1dcf88d26556f32f6c47c
vishal rai <vishalrai10797@gmail.com>
2025-11-12 12:45:45 +0530

    move the left side zeros

---
+++ restartEverythingDSA2026/MoveZeros.java
@@ -0,0 +1,24 @@
+package restartEverythingDSA2026;
+
+public class MoveZeros {
+
+    public static void  moveZeroes(int nums[]) {
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeroes(nums);
+        for (int n : nums) System.out.print(n + " ");
+    }
+    }
+
