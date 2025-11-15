48357ba9c621a15554a0895d7476b9b26b8c35ea
vishal rai <vishalrai10797@gmail.com>
2025-11-15 18:03:26 +0530

    ternary operators how to use

---
+++ restartEverythingDSA2026/Ternary.java
@@ -0,0 +1,10 @@
+package restartEverythingDSA2026;
+
+public class Ternary {
+    public static void main(String areg[]){
+        int a= 10;
+        int b = 5;
+        int max = (a > b) ? a:b;
+        System.out.println(max);
+    }
+}
