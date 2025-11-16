4b678f3cd96691164a4a2f581ef2aef7ebd742bc
vishal rai <vishalrai10797@gmail.com>
2025-11-16 19:08:26 +0530

    reursion

--- restartEverythingDSA2026/Recursion1.java
+++ restartEverythingDSA2026/Recursion1.java
@@ -2,6 +2,9 @@ package restartEverythingDSA2026;
 
 public class Recursion1 {
 
+
+
+
     public static void sayHello(int n){
         if(n == 0){
             return; // base case
@@ -10,8 +13,58 @@ public class Recursion1 {
         sayHello(n-1);
     }
 
+
+
+
+
+    public static int fact(int n){
+        if(n==1){
+            return 1;
+        }
+        return n * fact(n-1);
+    }
+
+
+
+    public static int fib(int n){
+        if(n<=1){
+            return n;
+        }
+        return fib(n-1) +fib(n-1);
+    }
+
+
+
+
+    public static int sum(int n){
+        if(n == 0){
+            return 0;
+        }
+        return (n%10)+sum(n/10);
+    }
+
+    public static int reverse(int n, int rev) {
+        if (n == 0) return rev;
+        return reverse(n / 10, rev * 10 + (n % 10));
+    }
+
+
     public static void main(String[] args) {
         sayHello(3);
+
+        int n= 5;
+        System.out.println(fact(n));
+
+        int x=5;
+        System.out.println(fib(x));
+
+
+        int b=123;
+        System.out.println(sum(b));
+
+
+
+
     }
 
 }
