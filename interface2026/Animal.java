94f2ad0d3f1c4a0f1fdc096064838c6ce132724a
vishal rai <vishalrai10797@gmail.com>
2025-11-16 17:44:04 +0530

    interface

---
+++ interface2026/Animal.java
@@ -0,0 +1,6 @@
+package interface2026;
+
+public interface Animal {
+    void eat();
+    void sleep();
+}
---
+++ interface2026/Main.java
@@ -0,0 +1,9 @@
+package interface2026;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog d= new Dog();
+        d.eat();
+        d.sleep();
+    }
+}
