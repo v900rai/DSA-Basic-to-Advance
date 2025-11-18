b28bdbbfb1a1c615e24beb0c79d6d042467c00c1
vishal rai <vishalrai10797@gmail.com>
2025-11-18 10:07:47 +0530

    substring in java

---
+++ restartEverythingDSA2026/JavaStringAllMethods.java
@@ -0,0 +1,9 @@
+package restartEverythingDSA2026;
+
+public class JavaStringAllMethods {
+    public static void main(String[] args) {
+        String s = "HelloWorld";
+        System.out.println(s.substring(5));      // World
+        System.out.println(s.substring(0, 5));
+    }
+}
