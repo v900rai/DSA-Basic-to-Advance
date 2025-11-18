d0db3d4d1c5ed486eeb1846bd65d0c71b4bf8f5e
vishal rai <vishalrai10797@gmail.com>
2025-11-18 10:33:15 +0530

    bubble sort

---
+++ restartEverythingDSA2026/BubbleSort.java
@@ -0,0 +1,22 @@
+package restartEverythingDSA2026;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+        public static void bubble(int[] arr) {
+            for (int i = 0; i < arr.length; i++)
+                for (int j = 1; j < arr.length - i; j++)
+                    if (arr[j] < arr[j - 1]) {
+                        int t = arr[j];
+                        arr[j] = arr[j - 1];
+                        arr[j - 1] = t;
+                    }
+        }
+
+        public static void main(String[] args) {
+            int[] arr = {5, 1, 4, 2};
+            bubble(arr);
+            System.out.println(Arrays.toString(arr));
+        }
+    }
