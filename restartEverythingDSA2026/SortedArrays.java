f3760167b8123931942b7128a82ac485f9c0d31c
vishal rai <vishalrai10797@gmail.com>
2025-11-18 10:46:01 +0530

    isSorted are not

---
+++ restartEverythingDSA2026/SortedArrays.java
@@ -0,0 +1,17 @@
+package restartEverythingDSA2026;
+
+public class SortedArrays {
+       public static boolean isSorted(int[] arr){
+            for(int i = 1; i<arr.length; i++){
+                if(arr[i] <arr[i-1])
+                    return false;
+            }
+            return true;
+       }
+        public static void main(String[] args) {
+            int[] arr = {1,2,3,4};
+            System.out.println(isSorted(arr));
+        }
+    }
+
+
