2a0489224df9fe9da12ef5cc700625b01653619b
vishal rai <vishalrai10797@gmail.com>
2025-11-18 11:03:06 +0530

    missing number first u can calculate total sum and

---
+++ restartEverythingDSA2026/MissingNumber.java
@@ -0,0 +1,19 @@
+package restartEverythingDSA2026;
+
+public class MissingNumber {
+
+        public static void main(String[] args) {
+            int[] arr = {1,2,3,4,5,7};
+
+            int n = arr.length + 1;
+            int total = n * (n + 1) / 2;
+
+            int sum = 0;
+           for(int i=0; i<arr.length; i++){
+               sum = sum+arr[i];
+           }
+            System.out.println(total - sum);
+        }
+    }
+
+
