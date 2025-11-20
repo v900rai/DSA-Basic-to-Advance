94a173748e5d53805b08e94d0795dcf1563c04b7
vishal rai <vishalrai10797@gmail.com>
2025-11-20 19:18:20 +0530

    butterflyPattern

---
+++ restartEverythingDSA2026/ButterflyPattern.java
@@ -0,0 +1,28 @@
+package restartEverythingDSA2026;
+
+public class ButterflyPattern {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++)
+                System.out.print("*");
+            for(int s=1;s<=2*(n-i);s++)
+                System.out.print(" ");
+            for(int j=1;j<=i;j++)
+                System.out.print("*");
+            System.out.println();
+        }
+
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++)
+                System.out.print("*");
+            for(int s=1;s<=2*(n-i);s++)
+                System.out.print(" ");
+            for(int j=1;j<=i;j++)
+                System.out.print("*");
+            System.out.println();
+        }
+
+
+    }
+}
