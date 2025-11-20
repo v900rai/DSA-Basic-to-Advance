690f1e7f162d4dee26bab08ef40ac1cb37e5b06d
vishal rai <vishalrai10797@gmail.com>
2025-11-20 19:27:58 +0530

    pyramid Of Number

---
+++ restartEverythingDSA2026/PyramidofNumbers.java
@@ -0,0 +1,20 @@
+package restartEverythingDSA2026;
+
+public class PyramidofNumbers {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int s=1;s<=n-i;s++)
+                System.out.print(" ");
+
+            for(int j=1;j<=i;j++)
+                System.out.print(j);
+
+            for(int j=i-1;j>=1;j--)
+                System.out.print(j);
+
+            System.out.println();
+        }
+
+    }
+}
