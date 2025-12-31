0f378d742cba6cb003b60810a482a62d731d6865
vishal rai <vishalrai10797@gmail.com>
2025-12-31 12:55:30 +0530

    bitManipulatioal

---
+++ restartEverythingDSA2026/BitManipulationEvenOrOdd.java
@@ -0,0 +1,14 @@
+package restartEverythingDSA2026;
+
+public class BitManipulationEvenOrOdd {
+    public static void main(String[] args) {
+        int n = 7;
+        if((n & 1)==0){
+            System.out.println("Even number ");
+        }
+        else{
+            System.out.println("Odd number ");
+
+        }
+    }
+}
