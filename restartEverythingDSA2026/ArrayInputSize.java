f27e17eb59eef167846159e237b8bc38a4303b68
vishal rai <vishalrai10797@gmail.com>
2025-12-31 12:49:40 +0530

    arrays are taking user in put and out put

---
+++ restartEverythingDSA2026/ArrayInputSize.java
@@ -0,0 +1,28 @@
+package restartEverythingDSA2026;
+
+import java.util.Scanner;
+
+public class ArrayInputSize {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter array size: ");
+            int n = sc.nextInt();
+
+            int[] arr = new int[n];
+
+            System.out.println("Enter array elements:");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            System.out.println("Array elements are:");
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+
+            sc.close();
+        }
+    }
