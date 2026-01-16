cdebad2c4bbdf4e969ce7d0eff3cb81a036b9e4f
vishal rai <vishalrai10797@gmail.com>
2026-01-16 12:39:54 +0530

    first bitwise operators

---
+++ restartEverythingDSA2026/BitwiseOperators.java
@@ -0,0 +1,15 @@
+package restartEverythingDSA2026;
+
+public class BitwiseOperators {
+    public static void main(String[] args) {
+
+        int a = 5;  // 0101
+        int b = 3;  // 0011
+
+        System.out.println(a & b); // 1
+        System.out.println(a | b); // 7
+        System.out.println(a ^ b); // 6
+        System.out.println(a << 1); // 10
+        System.out.println(a >> 1); // 2
+    }
+}
