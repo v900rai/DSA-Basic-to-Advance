94d26064dc18801f31367f0901bc209a95321bf4
vishal rai <vishalrai10797@gmail.com>
2026-01-23 15:40:45 +0530

    remove the wite space

---
+++ restartEverythingDSA2026/RemoveWhitespaces.java
@@ -0,0 +1,8 @@
+package restartEverythingDSA2026;
+
+public class RemoveWhitespaces {
+    public static void main(String[] args) {
+        String str = "java is easy";
+        System.out.println(str.replace(" ",""));
+    }
+}
