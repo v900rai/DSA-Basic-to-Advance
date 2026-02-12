f1691053c73d58200dc5d62e5ed0e4c90e10a253
vishal rai <vishalrai10797@gmail.com>
2026-02-12 12:02:22 +0530

    What is constructor in java their are two types in parameters and default

---
+++ restartEverythingDSA2026/DefaultConstructor.java
@@ -0,0 +1,15 @@
+package restartEverythingDSA2026;
+
+public class DefaultConstructor {
+
+
+    DefaultConstructor () {
+            System.out.println("Default constructor called");
+        }
+
+        public static void main(String[] args) {
+            DefaultConstructor d = new DefaultConstructor();
+        }
+    }
+
+
