a0608104df2d68b52417cd5b54906be7964c40b6
vishal rai <vishalrai10797@gmail.com>
2026-03-05 12:35:27 +0530

    java slts

---
+++ restartEverythingDSA2026/JavaLTS.java
@@ -0,0 +1,11 @@
+package restartEverythingDSA2026;
+
+public class JavaLTS {
+    /*public static void main(String[] args) {
+        System.out.println("Hello vishal rai");
+    }*/
+    public static void main() {
+        System.out.println("Hello vishal rai");
+
+    }
+}
