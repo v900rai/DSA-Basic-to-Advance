9c479a1ea76990f2c881d5ea5fa537bc55880b41
vishal rai <vishalrai10797@gmail.com>
2026-03-05 12:51:37 +0530

    absoluteValue

---
+++ restartEverythingDSA2026/AbsoluteValue.java
@@ -0,0 +1,9 @@
+package restartEverythingDSA2026;
+
+public class AbsoluteValue {
+
+    public static void main(String[] args) {
+        int n = -6;
+        System.out.println(Math.abs(n));
+    }
+}
