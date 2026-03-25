0c0ec6b10910a5de36d6e70affe68967a82ea5d5
vishal rai <vishalrai10797@gmail.com>
2026-03-25 20:21:53 +0530

    Frequency count

---
+++ restartEverythingDSAMarch2026/FrequencyCount.java
@@ -0,0 +1,16 @@
+package restartEverythingDSAMarch2026;
+
+import java.util.HashMap;
+
+public class FrequencyCount {
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,3};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num: arr){
+            map.put(num , map.getOrDefault(num,0)+1);
+        }
+
+        System.out.println(map);
+    }
+}
