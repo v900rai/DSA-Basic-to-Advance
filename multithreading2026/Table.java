eadb1ade3c8ad4ff62744acd2b7245d32cc4cbba
vishal rai <vishalrai10797@gmail.com>
2026-06-23 12:10:27 +0530

    plpjohgilufdyr

---
+++ multithreading2026/Table.java
@@ -0,0 +1,14 @@
+package multithreading2026;
+
+public class Table {
+
+        public static void main(String[] args) {
+
+            int n = 5;
+
+            for(int i = 1; i <= 10; i++) {
+                System.out.println(n + " x " + i + " = " + (n * i));
+            }
+        }
+    }
+
